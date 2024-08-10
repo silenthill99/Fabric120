ff9d9e05ea0118251d91ae37d4fd44ee6851a8e9
silenthill99 <florian.graziani@sfr.fr>
2024-08-11 00:47:32 +0200

    Initial commit

---
+++ src/main/java/fr/silenthill99/Main.java
@@ -0,0 +1,19 @@
+package fr.silenthill99;
+
+import fr.silenthill99.init.ModItemGroups;
+import fr.silenthill99.init.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static final String MOD_ID = "test_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		ModItemGroups.registerModItemGroups();
+	}
+}
---
+++ src/main/java/fr/silenthill99/MainClient.java
@@ -0,0 +1,10 @@
+package fr.silenthill99;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
