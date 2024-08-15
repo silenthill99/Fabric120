b6d9d8e80d590c42029ae7d951777691f8439ef7
BuildTools <unconfigured@null.spigotmc.org>
2024-08-15 10:23:23 +0200

    Finition de l'initiation aux data generator

--- src/main/java/fr/silenthill99/test_mod/datagen/ModLootTableProvider.java
+++ src/main/java/fr/silenthill99/test_mod/datagen/ModLootTableProvider.java
@@ -4,6 +4,14 @@ import fr.silenthill99.test_mod.init.ModBlocks;
 import fr.silenthill99.test_mod.init.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
 
@@ -15,9 +23,16 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
     public void generate() {
         addDrop(ModBlocks.RUBY_BLOCK);
         addDrop(ModBlocks.RAW_RUBY_BLOCK);
-        addDrop(ModBlocks.RUBY_ORE, block -> oreDrops(block, ModItems.RAW_RUBY));
-        addDrop(ModBlocks.DEEPSLATE_RUBY_ORE, block -> oreDrops(block, ModItems.RAW_RUBY));
-        addDrop(ModBlocks.NETHER_RUBY_ORE, block -> oreDrops(block, ModItems.RAW_RUBY));
-        addDrop(ModBlocks.END_STONE_RUBY_ORE, block -> oreDrops(block, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.SOUND_BLOCK);
+        addDrop(ModBlocks.RUBY_ORE, block -> copperLikeOreDrops(block, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.DEEPSLATE_RUBY_ORE, block -> copperLikeOreDrops(block, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.NETHER_RUBY_ORE, block -> copperLikeOreDrops(block, ModItems.RAW_RUBY));
+        addDrop(ModBlocks.END_STONE_RUBY_ORE, block -> copperLikeOreDrops(block, ModItems.RAW_RUBY));
+    }
+
+    public LootTable.Builder copperLikeOreDrops(Block drop, Item item) {
+        return dropsWithSilkTouch(drop, this.applyExplosionDecay(drop, ItemEntry.builder(item)
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0F, 5.0F)))
+                .apply(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE))));
     }
 }
--- src/main/java/fr/silenthill99/test_mod/datagen/ModRecipeProvider.java
+++ src/main/java/fr/silenthill99/test_mod/datagen/ModRecipeProvider.java
@@ -4,34 +4,46 @@ import fr.silenthill99.test_mod.init.ModBlocks;
 import fr.silenthill99.test_mod.init.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.advancement.criterion.InventoryChangedCriterion;
-import net.minecraft.data.server.recipe.CookingRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
-import net.minecraft.recipe.Ingredient;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
+    private static List<ItemConvertible> RUBY_SMELTABLES = List.of(
+            ModItems.RAW_RUBY,
+            ModBlocks.RUBY_ORE,
+            ModBlocks.DEEPSLATE_RUBY_ORE,
+            ModBlocks.NETHER_RUBY_ORE,
+            ModBlocks.END_STONE_RUBY_ORE
+    );
     public ModRecipeProvider(FabricDataOutput output) {
         super(output);
     }
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RUBY, RecipeCategory.DECORATIONS,
+                ModBlocks.RUBY_BLOCK);
 
-        CookingRecipeJsonBuilder.createBlasting(Ingredient.ofItems(ModItems.RAW_RUBY), RecipeCategory.MISC, ModItems.RUBY,
-                        0.7f, 100)
-                .criterion("unlock", InventoryChangedCriterion.Conditions.items(ModItems.RAW_RUBY))
-                .group("ruby")
-                .offerTo(exporter, "ruby_from_blasting_raw_ruby");
-        CookingRecipeJsonBuilder.createSmelting(Ingredient.ofItems(ModItems.RAW_RUBY), RecipeCategory.MISC, ModItems.RUBY,
-                        0.7f, 200)
-                .criterion("unlock", InventoryChangedCriterion.Conditions.items(ModItems.RAW_RUBY))
-                .group("ruby")
-                .offerTo(exporter, "ruby_from_smelting_raw_ruby");
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RUBY, RecipeCategory.MISC,
-                ModBlocks.RUBY_BLOCK, "ruby_block_from_ruby", null, "ruby_from_ruby_block",
-                null);
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RUBY)
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .input('S', Items.STONE)
+                .input('R', ModItems.RUBY)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_RUBY)));
+
     }
 }
