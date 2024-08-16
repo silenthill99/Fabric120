cb10f0788c4d01a6640646ea0095b8397927f3c3
BuildTools <unconfigured@null.spigotmc.org>
2024-08-16 23:50:08 +0200

    Ajout des armures

--- src/main/java/fr/silenthill99/test_mod/init/ModItems.java
+++ src/main/java/fr/silenthill99/test_mod/init/ModItems.java
@@ -2,6 +2,7 @@ package fr.silenthill99.test_mod.init;
 
 import fr.silenthill99.test_mod.Main;
 import fr.silenthill99.test_mod.custom.item.MetalDetectorItem;
+import fr.silenthill99.test_mod.utils.ModArmorMaterials;
 import fr.silenthill99.test_mod.utils.ModToolMaterial;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
@@ -34,6 +35,15 @@ public class ModItems {
             new SwordItem(ModToolMaterial.RUBY, 5, 3, new FabricItemSettings()));
     public static final Item RUBY_HOE = registerItem("ruby_hoe",
             new HoeItem(ModToolMaterial.RUBY, 0, 0, new FabricItemSettings()));
+
+    public static final Item RUBY_HELMET = registerItem("ruby_helmet",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
+    public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+    public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+    public static final Item RUBY_BOOTS = registerItem("ruby_boots",
+            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.BOOTS, new FabricItemSettings()));
 
     private static void addItemsToIngredientTabItemGroups(FabricItemGroupEntries entries) {
         entries.add(RUBY);
---
+++ src/main/java/fr/silenthill99/test_mod/utils/ModArmorMaterials.java
@@ -0,0 +1,80 @@
+package fr.silenthill99.test_mod.utils;
+
+import fr.silenthill99.test_mod.Main;
+import fr.silenthill99.test_mod.init.ModItems;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    RUBY("ruby", 25, new int[] { 3, 8, 6, 3 }, 19,
+            SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 2, 0.1f, () -> Ingredient.ofItems(ModItems.RUBY))
+    ;
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredientSupplier;
+
+    private final int[] BASE_DURABILITY = { 11, 16, 15, 13 };
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound,
+                      float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredientSupplier) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredientSupplier = repairIngredientSupplier;
+    }
+
+    @Override
+    public int getDurability(ArmorItem.Type type) {
+        return BASE_DURABILITY[type.ordinal()] * durabilityMultiplier;
+    }
+
+    @Override
+    public int getProtection(ArmorItem.Type type) {
+        return protectionAmounts[type.ordinal()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredientSupplier.get();
+    }
+
+    @Override
+    public String getName() {
+        return new Identifier(Main.MOD_ID, name).toString();
+    }
+
+    @Override
+    public float getToughness() {
+        return toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return knockbackResistance;
+    }
+}
