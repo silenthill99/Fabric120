ff96452482df247bb839058cfa482342ea249c1a
silenthill99 <florian.graziani@sfr.fr>
2024-09-03 23:43:08 +0200

    Ajout des loot tables dans le sable suspect

--- src/main/java/fr/silenthill99/test_mod/utils/ModLootTablesModifier.java
+++ src/main/java/fr/silenthill99/test_mod/utils/ModLootTablesModifier.java
@@ -3,17 +3,24 @@ package fr.silenthill99.test_mod.utils;
 import fr.silenthill99.test_mod.init.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ModLootTablesModifier {
 
     private static final Identifier JUNGLE_TEMPLE_ID = new Identifier("chests/jungle_temple");
     private static final Identifier CREEPER_ID = new Identifier("entities/creeper");
+    private static final Identifier SUSPICIOUS_SAND_ID = new Identifier("archaeology/desert_pyramid");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -36,6 +43,17 @@ public class ModLootTablesModifier {
                 tableBuilder.pool(poolBuilder);
             }
         });
-    }
 
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) -> {
+            if (SUSPICIOUS_SAND_ID.equals(id)) {
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+
+                LootPool.Builder pool = LootPool.builder().with(entries);
+                return LootTable.builder().pool(pool).build();
+            }
+            return null;
+        });
+    }
 }
